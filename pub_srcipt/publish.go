ab142dc944cfa67fef9cd16e11e48b3e10facb5b
Ccaswell42 <dmitriykuzin11@yandex.ru>
2022-08-16 21:31:12 +0300

    add env

---
+++ pub_srcipt/publish.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nats-io/stan.go"
+	"io/ioutil"
+	"log"
+	"os"
+	"wildberries/config"
+)
+
+func main() {
+	conf, err := config.GetConfig() // передать переменную в функции
+	if err != nil {
+		log.Fatal("error during config downloading: ", err)
+	}
+	sc, err := stan.Connect(conf.StanClusterId, "clientID2")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err = sc.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	mod, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = sc.Publish(conf.Subject, mod)
+	if err != nil {
+		log.Println(err)
+	}
+}
